fa381e0b757e9ec9e6f2ee76808175727d04021f
Sachin <sdole@genvega.com>
2015-08-19 05:59:45 -0500

    inited cookbook

---
+++ tvarit-cookbook/metadata.rb
@@ -0,0 +1,9 @@
+name             'tvarit-cookbook'
+maintainer       'YOUR_NAME'
+maintainer_email 'YOUR_EMAIL'
+license          'All rights reserved'
+description      'Installs/Configures tvarit-cookbook'
+long_description 'Installs/Configures tvarit-cookbook'
+
+version          '0.1.0'
+
---
+++ tvarit-cookbook/recipes/default.rb
@@ -0,0 +1,8 @@
+#
+# Cookbook Name:: tvarit-cookbook
+# Recipe:: default
+#
+# Copyright (C) 2015 YOUR_NAME
+#
+# All rights reserved - Do Not Redistribute
+#
