c3aa0c633147dd967d51c44e237a4ba1f578c05e
Sachin <sdole@genvega.com>
2015-08-19 11:44:50 -0500

    the plugin creates stacks

--- tvarit-maven-plugin/src/main/java/com/tvarit/plugin/TvaritMojo.java
+++ tvarit-maven-plugin/src/main/java/com/tvarit/plugin/TvaritMojo.java
@@ -1,10 +1,9 @@
 package com.tvarit.plugin;
 
 import com.amazonaws.auth.BasicAWSCredentials;
+import com.amazonaws.regions.Regions;
 import com.amazonaws.services.opsworks.AWSOpsWorksClient;
-import com.amazonaws.services.opsworks.model.DescribeStacksRequest;
-import com.amazonaws.services.opsworks.model.DescribeStacksResult;
-import com.amazonaws.services.opsworks.model.Stack;
+import com.amazonaws.services.opsworks.model.*;
 import org.apache.maven.plugin.AbstractMojo;
 import org.apache.maven.plugin.MojoExecutionException;
 import org.apache.maven.plugin.MojoFailureException;
@@ -12,30 +11,41 @@ import org.apache.maven.plugins.annotations.LifecyclePhase;
 import org.apache.maven.plugins.annotations.Mojo;
 import org.apache.maven.plugins.annotations.Parameter;
 
+import java.util.List;
 import java.util.stream.Collectors;
 
 @Mojo(name = "hello", defaultPhase = LifecyclePhase.COMPILE)
 public class TvaritMojo extends AbstractMojo {
 
-    @Parameter(alias = "stack", required = true)
+    @Parameter(required = true, alias = "stack")
     private String stackName;
 
-    @Parameter(required = true, property = "accessKey")
+    @Parameter(required = true, alias = "accessKey")
     private String accessKey;
-    @Parameter(required = true, property = "secretKey")
+    @Parameter(required = true, alias = "secretKey",readonly = true)
     private String secretKey;
-
+    @Parameter(required = true,alias = "role_arn")
+    private String roleArn;
+    @Parameter(required = true,alias="instance_profile_arn")
+    private String instanceProfileArn;
 
     @Override
     public void execute() throws MojoExecutionException, MojoFailureException {
         getLog().debug("Starting " + this.getClass().getSimpleName() + " execution ");
-        getLog().debug("stackName:-" + stackName + "-");
         AWSOpsWorksClient awsOpsWorksClient = new AWSOpsWorksClient(new BasicAWSCredentials(accessKey, secretKey));
         DescribeStacksRequest describeStacksRequest = new DescribeStacksRequest();
-//        describeStacksRequest.withStackIds(stackName);
         DescribeStacksResult describeStacksResult = awsOpsWorksClient.describeStacks(describeStacksRequest);
-
-        getLog().debug(describeStacksResult.getStacks().stream().map(Stack::getName).collect(Collectors.toList()).toString());
+        List<String> stacksFound = describeStacksResult.getStacks().stream().map(Stack::getName).collect(Collectors.toList());
+        if (stacksFound.isEmpty() || !stacksFound.contains(stackName)) {
+            getLog().debug("No stacks found! Will create!");
+            CreateStackRequest createStackRequest = new CreateStackRequest();
+            createStackRequest.withName(stackName).withServiceRoleArn(roleArn).withRegion(Regions.US_EAST_1.getName()).withDefaultInstanceProfileArn(instanceProfileArn);
+            CreateStackResult createStackResult = awsOpsWorksClient.createStack(createStackRequest);
+            getLog().debug("Created stack! "  + createStackResult.getStackId());
+        } else {
+            getLog().debug("Found stacks: " + stacksFound.toString());
+        }
+        getLog().debug("Done!");
     }
 
 }
