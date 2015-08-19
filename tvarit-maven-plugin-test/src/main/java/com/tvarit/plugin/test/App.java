f908d1cac88a99af5e4a4206d05da3bde86845f1
Sachin <sdole@genvega.com>
2015-08-19 07:37:02 -0500

    test app runs. does not have distribution tag yet

---
+++ tvarit-maven-plugin-test/src/main/java/com/tvarit/plugin/test/App.java
@@ -0,0 +1,11 @@
+package com.tvarit.plugin.test;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+/**
+ * Created by sachi_000 on 8/19/2015.
+ */
+@ApplicationPath("rest")
+public class App extends Application {
+}
