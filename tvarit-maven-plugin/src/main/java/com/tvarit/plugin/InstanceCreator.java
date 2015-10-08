a65535fdc8f966c0321daa8f8c9cee1799bd280e
Sachin <sdole@genvega.com>
2015-10-07 19:03:04 -0500

    instance now works and chef kicks off. however chef fails saying that it could not find java... this is got to be some config issue. will check up.

--- tvarit-maven-plugin/src/main/java/com/tvarit/plugin/InstanceCreator.java
+++ tvarit-maven-plugin/src/main/java/com/tvarit/plugin/InstanceCreator.java
@@ -2,10 +2,7 @@ package com.tvarit.plugin;
 
 import com.amazonaws.services.ec2.model.InstanceType;
 import com.amazonaws.services.opsworks.AWSOpsWorksClient;
-import com.amazonaws.services.opsworks.model.AutoScalingType;
-import com.amazonaws.services.opsworks.model.CreateInstanceRequest;
-import com.amazonaws.services.opsworks.model.CreateInstanceResult;
-import com.amazonaws.services.opsworks.model.RootDeviceType;
+import com.amazonaws.services.opsworks.model.*;
 
 /**
  * Created by sachi_000 on 9/28/2015.
@@ -13,13 +10,20 @@ import com.amazonaws.services.opsworks.model.RootDeviceType;
 public class InstanceCreator {
     public String create(TvaritMojo tvaritMojo, AWSOpsWorksClient awsOpsWorksClient, InfrastructureIds infrastructureIds) {
         final CreateInstanceRequest createInstanceRequest = new CreateInstanceRequest();
+        final BlockDeviceMapping blockDeviceMapping = new BlockDeviceMapping();
+        final EbsBlockDevice ebsBlockDevice = new EbsBlockDevice();
+        ebsBlockDevice
+                .withVolumeSize(32)
+                .withDeleteOnTermination(true)
+                .withVolumeType(VolumeType.Gp2);
+        blockDeviceMapping.withEbs(ebsBlockDevice).withDeviceName("ROOT_DEVICE");
         createInstanceRequest.
                 withStackId(infrastructureIds.getStackId()).
                 withLayerIds(infrastructureIds.getLayerId()).
-                withInstanceType(InstanceType.T2Micro.toString()).
+                withInstanceType(InstanceType.M3Medium.toString()).
                 withRootDeviceType(RootDeviceType.Ebs).
                 withSubnetId(infrastructureIds.getSubnetId()).
-                withAutoScalingType(AutoScalingType.Load);
+                withBlockDeviceMappings(blockDeviceMapping);
         final CreateInstanceResult createInstanceResult = awsOpsWorksClient.createInstance(createInstanceRequest);
         tvaritMojo.getLog().debug("started instance with id: " + createInstanceResult.getInstanceId());
         return createInstanceResult.getInstanceId();
