bf832aaa4d788add49faf054569a9123b0d20a2a
Sachin <sdole@genvega.com>
2015-10-10 19:24:38 -0500

    starting to debug this recipe on aws.

--- tvarit-cookbook/recipes/default.rb
+++ tvarit-cookbook/recipes/default.rb
@@ -9,4 +9,8 @@
 Chef::Log.debug('aha!')
 Chef::Log.fatal('aha fatal!')
 include_recipe 'java'
+Chef::Log.debug('after include java!')
+Chef::Log.fatal('after include java!')
 include_recipe 'wildfly::install'
+Chef::Log.debug('after include java!!')
+Chef::Log.fatal('after include wildfly')
