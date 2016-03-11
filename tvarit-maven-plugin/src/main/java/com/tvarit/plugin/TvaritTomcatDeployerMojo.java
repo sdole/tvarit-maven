ff88861f749d053d6a403a4b50f8b32a7a2302f0
Sachin <sdole@genvega.com>
2016-03-11 09:34:17 -0600

    tvarit-maven-13 finish the lambda function to deploy instances
    oops fixing compile error

--- tvarit-maven-plugin/src/main/java/com/tvarit/plugin/TvaritTomcatDeployerMojo.java
+++ tvarit-maven-plugin/src/main/java/com/tvarit/plugin/TvaritTomcatDeployerMojo.java
@@ -20,6 +20,8 @@
 package com.tvarit.plugin;
 
 import com.amazonaws.auth.BasicAWSCredentials;
+import com.amazonaws.services.cloudformation.AmazonCloudFormationClient;
+import com.amazonaws.services.cloudformation.model.CreateStackRequest;
 import com.amazonaws.services.s3.AmazonS3Client;
 import com.amazonaws.services.s3.model.PutObjectRequest;
 import com.amazonaws.services.s3.model.PutObjectResult;
@@ -40,13 +42,28 @@ public class TvaritTomcatDeployerMojo extends AbstractMojo {
     private String secretKey;
     @Parameter(required = true)
     private String bucketName;
+    @Parameter(required = true)
+    private String projectName;
+    @Parameter
+    private String templateUrl;
 
     @Override
     public void execute() throws MojoExecutionException, MojoFailureException {
-        AmazonS3Client s3Client = new AmazonS3Client(new BasicAWSCredentials(accessKey, secretKey));
+        final BasicAWSCredentials awsCredentials = new BasicAWSCredentials(accessKey, secretKey);
+        AmazonS3Client s3Client = new AmazonS3Client(awsCredentials);
         final MavenProject project = (MavenProject) this.getPluginContext().getOrDefault("project", null);
         final File warFile = project.getArtifact().getFile();
         final String key = "war/" + project.getGroupId() + "/" + project.getArtifactId() + "/" + project.getVersion() + "/" + warFile.getName();
         final PutObjectResult putObjectResult = s3Client.putObject(new PutObjectRequest(bucketName, key, warFile));
+        AmazonCloudFormationClient amazonCloudFormationClient = new AmazonCloudFormationClient(awsCredentials);
+        final CreateStackRequest createStackRequest = new CreateStackRequest();
+        createStackRequest.withStackName(projectName + "-instance-" + project.getVersion());
+        if (templateUrl == null) {
+            String templateBody = new TemplateReader().readTemplate("/newinstance.template");
+            createStackRequest.withTemplateBody(templateBody);
+        } else {
+            createStackRequest.withTemplateURL(templateUrl);
+        }
+        amazonCloudFormationClient.createStack(createStackRequest);
     }
 }
