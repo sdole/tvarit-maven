0d310057e050edcc24d99e0775890486383f3865
Sachin <sdole@genvega.com>
2016-03-26 13:56:47 -0500

    started work on lambda. this check in moves all templates to s3 in newly created customer bucket. subsequent runs should get templates from S3

--- tvarit-maven-plugin/src/main/java/com/tvarit/plugin/NewInfrastructureMojo.java
+++ tvarit-maven-plugin/src/main/java/com/tvarit/plugin/NewInfrastructureMojo.java
@@ -24,24 +24,26 @@ import com.amazonaws.auth.BasicAWSCredentials;
 import com.amazonaws.services.cloudformation.AmazonCloudFormationClient;
 import com.amazonaws.services.cloudformation.model.Capability;
 import com.amazonaws.services.cloudformation.model.CreateStackRequest;
+import com.amazonaws.services.s3.AmazonS3Client;
+import com.amazonaws.services.s3.model.ObjectMetadata;
+import com.amazonaws.services.s3.model.PutObjectRequest;
 import org.apache.maven.plugin.AbstractMojo;
 import org.apache.maven.plugin.MojoExecutionException;
 import org.apache.maven.plugin.MojoFailureException;
 import org.apache.maven.plugins.annotations.Mojo;
 import org.apache.maven.plugins.annotations.Parameter;
+import org.apache.maven.project.MavenProject;
 
-@Mojo(name = "make-infra")
-@Deprecated
-/**
- * @deprecated to be removed in version 1.0.0
- */
-public class InfrastructureMojo extends AbstractMojo {
+import java.io.InputStream;
+
+@Mojo(name = "make-infrastructure")
+public class NewInfrastructureMojo extends AbstractMojo {
     @Parameter(required = true, readonly = true, property = "mySecretKey")
     private String secretKey;
     @Parameter(required = true, readonly = true, property = "myAccessKey")
     private String accessKey;
     @Parameter
-    private String templateUrl;
+    private String templateBodyFile;
     @Parameter(required = true)
     private String projectName;
     @Parameter(required = true)
@@ -52,22 +54,42 @@ public class InfrastructureMojo extends AbstractMojo {
     @Override
     public void execute() throws MojoExecutionException, MojoFailureException {
         getLog().debug("Starting " + this.getClass().getSimpleName() + " execution ");
-        getLog().warn("This goal has been deprecated and may be removed without notice. Please use the goal 'make-infrastructure' instead.");
         final BasicAWSCredentials awsCredentials = new BasicAWSCredentials(accessKey, secretKey);
         AmazonCloudFormationClient amazonCloudFormationClient = new AmazonCloudFormationClient(awsCredentials);
         final com.amazonaws.services.cloudformation.model.Parameter domainNameParameter = new com.amazonaws.services.cloudformation.model.Parameter().withParameterKey("domainName").withParameterValue(this.domainName);
         final com.amazonaws.services.cloudformation.model.Parameter projectNameParameter = new com.amazonaws.services.cloudformation.model.Parameter().withParameterKey("projectName").withParameterValue(this.projectName);
-        final com.amazonaws.services.cloudformation.model.Parameter bucketNameParameter = new com.amazonaws.services.cloudformation.model.Parameter().withParameterKey("bucketName").withParameterValue("tvarit-" + this.bucketName);
-        final CreateStackRequest createStackRequest = new CreateStackRequest().withCapabilities(Capability.CAPABILITY_IAM).withStackName(projectName + "-infra").withParameters(domainNameParameter, projectNameParameter, bucketNameParameter);
-        if (templateUrl == null) {
-            final String template = new TemplateReader().readTemplate("/vpc-infra.template");
-            createStackRequest.withTemplateBody(template);
-        } else {
-            createStackRequest.withTemplateURL(templateUrl);
-        }
-        new StackMaker().makeStack(createStackRequest, amazonCloudFormationClient, getLog());
+        this.bucketName = "tvarit-" + this.bucketName;
+        final com.amazonaws.services.cloudformation.model.Parameter bucketNameParameter = new com.amazonaws.services.cloudformation.model.Parameter().withParameterKey("bucketName").withParameterValue(this.bucketName);
+        final String template;
 
+        if (templateBodyFile == null) templateBodyFile = "/vpc-infra.template";
+        else if (!templateBodyFile.startsWith("/")) templateBodyFile = "/" + templateBodyFile;
+
+        template = new TemplateReader().readTemplate(templateBodyFile);
+        final CreateStackRequest createStackRequest =
+                new CreateStackRequest().
+                        withCapabilities(Capability.CAPABILITY_IAM).
+                        withStackName(projectName + "-infra").
+                        withParameters(domainNameParameter, projectNameParameter, bucketNameParameter).
+                        withTemplateBody(template);
+        new StackMaker().makeStack(createStackRequest, amazonCloudFormationClient, getLog());
+        AmazonS3Client s3Client = new AmazonS3Client(awsCredentials);
+        final MavenProject project = (MavenProject) this.getPluginContext().getOrDefault("project", null);
+        this.uploadInfrastructureTemplate(s3Client, project, "/infrastructure.template", templateBodyFile);
+        this.uploadInfrastructureTemplate(s3Client, project, "/autoscaling.template", "/autoscaling.template");
+        this.uploadInfrastructureTemplate(s3Client, project, "/newinstance.template", "/newinstance.template");
         getLog().info("Finished completing stack");
+
+    }
+
+
+    private void uploadInfrastructureTemplate(AmazonS3Client s3Client, MavenProject project, String fileName, String templateBodyFileName) {
+        final ObjectMetadata infraTemplateMetadata = new ObjectMetadata();
+        infraTemplateMetadata.setContentType("application/json");
+        final String infraTemplateKeyName = "cfn-templates/" + project.getGroupId() + "/" + project.getArtifactId() + "/" + project.getVersion() + fileName;
+        final InputStream infraTemplateInputStream = this.getClass().getResourceAsStream(templateBodyFileName);
+        final PutObjectRequest putObjectRequest = new PutObjectRequest(this.bucketName, infraTemplateKeyName, infraTemplateInputStream, infraTemplateMetadata);
+        s3Client.putObject(putObjectRequest);
 
     }
 }
