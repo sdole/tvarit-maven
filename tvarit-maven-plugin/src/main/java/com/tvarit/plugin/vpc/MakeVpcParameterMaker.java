687628b890155a681e076302e68ef31c1edadf5d
sdole@genvega.com <sdole@genvega.com>
2016-06-23 09:09:30 -0500

    work in progress - new network template with new mojo

--- tvarit-maven-plugin/src/main/java/com/tvarit/plugin/vpc/MakeVpcParameterMaker.java
+++ tvarit-maven-plugin/src/main/java/com/tvarit/plugin/vpc/MakeVpcParameterMaker.java
@@ -1,11 +1,20 @@
 package com.tvarit.plugin.vpc;
 
 import com.amazonaws.services.cloudformation.model.Parameter;
+import com.tvarit.plugin.MakeVpcMojo;
+import com.tvarit.plugin.env.TvaritEnvironment;
+import edu.emory.mathcs.backport.java.util.Arrays;
 
 import java.util.List;
 
 class MakeVpcParameterMaker {
     List<Parameter> make() {
-        return null;
+        String projectName = TvaritEnvironment.getInstance().<MakeVpcMojo>getMojo().getProjectName();
+        String domainName = TvaritEnvironment.getInstance().<MakeVpcMojo>getMojo().getDomainName();
+        String bucketName = TvaritEnvironment.getInstance().<MakeVpcMojo>getMojo().getBucketName();
+        final com.amazonaws.services.cloudformation.model.Parameter bucketNameParm = new com.amazonaws.services.cloudformation.model.Parameter().withParameterKey("TvaritBucketNameParm").withParameterValue(domainName);
+        final com.amazonaws.services.cloudformation.model.Parameter projectNameParm = new com.amazonaws.services.cloudformation.model.Parameter().withParameterKey("ProjectNameParm").withParameterValue(projectName);
+        final com.amazonaws.services.cloudformation.model.Parameter tvaritBucketNameParm = new com.amazonaws.services.cloudformation.model.Parameter().withParameterKey("TvaritBucketNameParm").withParameterValue(bucketName);
+        return Arrays.<Parameter>asList(new Parameter[]{bucketNameParm, projectNameParm, tvaritBucketNameParm});
     }
 }
