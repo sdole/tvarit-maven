431bf941de389cc0e405783df4dfa0097a2abb0f
Sachin <sdole@genvega.com>
2016-06-26 07:56:56 -0500

    added a profile parameter to aws s3 plugin

--- tvarit-maven-plugin/src/main/java/com/tvarit/plugin/app/AppDeployerDelegate.java
+++ tvarit-maven-plugin/src/main/java/com/tvarit/plugin/app/AppDeployerDelegate.java
@@ -1,7 +1,11 @@
 package com.tvarit.plugin.app;
 
+import com.amazonaws.services.s3.AmazonS3Client;
+
 class AppDeployerDelegate {
     void deploy() {
+
+        AmazonS3Client s3Client = new AmazonS3Client();
 
 
         /*
