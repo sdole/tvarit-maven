765209a1975c7abf8c165f34d203877f57187a02
Sachin <sdole@genvega.com>
2016-07-21 21:55:41 -0500

    fixed errors and bugs in templates

--- tvarit-maven-plugin/src/main/java/com/tvarit/plugin/base/MakeBaseInfrastructureParameterMaker.java
+++ tvarit-maven-plugin/src/main/java/com/tvarit/plugin/base/MakeBaseInfrastructureParameterMaker.java
@@ -19,7 +19,7 @@ class MakeBaseInfrastructureParameterMaker {
         final Parameter projectNameParm = new Parameter().withParameterKey("ProjectNameParm").withParameterValue(projectName);
         final Parameter availabilityZonesParm = new Parameter().withParameterKey("AvailabilityZones").withParameterValue(availabilityZones);
         final Parameter elbHealthCheckAbsoluteUrlParm = new Parameter().withParameterKey("ElbHealthCheckAbsoluteUrl").withParameterValue("/to_be_fixed.html");
-        final Parameter sshKeyParm = new Parameter().withParameterKey("SshKey").withParameterValue(TvaritEnvironment.getInstance().<MakeBaseInfrastructureMojo>getMojo().getSshKeyPairName());
+        final Parameter sshKeyParm = new Parameter().withParameterKey("SshKeyPairName").withParameterValue(TvaritEnvironment.getInstance().<MakeBaseInfrastructureMojo>getMojo().getSshKeyPairName());
         String routerTemplateUrl;
         String networkTemplateUrl;
         try {
