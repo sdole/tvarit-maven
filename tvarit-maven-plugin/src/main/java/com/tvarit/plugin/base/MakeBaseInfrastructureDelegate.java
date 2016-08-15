aa6b9ef97ab6ac85c222362d86f63ca54eddb2cc
Sachin <sdole@genvega.com>
2016-08-14 21:22:27 -0500

    stack name rename

--- tvarit-maven-plugin/src/main/java/com/tvarit/plugin/base/MakeBaseInfrastructureDelegate.java
+++ tvarit-maven-plugin/src/main/java/com/tvarit/plugin/base/MakeBaseInfrastructureDelegate.java
@@ -21,7 +21,7 @@ public class MakeBaseInfrastructureDelegate {
         } catch (MalformedURLException e) {
             throw new RuntimeException(e);
         }
-        createVpcStackRequest.withTemplateURL(url.toString()).withStackName("tvarit-base-infrastructure2");
+        createVpcStackRequest.withTemplateURL(url.toString()).withStackName("tvarit-base-infrastructure");
         List<Parameter> makeVpcParameters = new MakeBaseInfrastructureParameterMaker().make();
         createVpcStackRequest.withParameters(makeVpcParameters).withCapabilities(Capability.CAPABILITY_IAM);
         amazonCloudFormationClient.createStack(createVpcStackRequest);
